fe5ea5e3527ddb7aede7f6b9e60c82b06a3c0a8d
Christie Hui <cwsh15@gmail.com>
2019-08-27 23:54:00 -0700

    finished 5/6 comprehensive operations

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -3,8 +3,9 @@ $(document).ready(initializeApp);
 var calculationArray = [];
 var displayArray = [];
 var stringNumberToPush = "";
-var calculationResult = null;
+var answer = null;
 var inputtedNumber = "";
+// var displayText = null;
 
 function initializeApp(){
   applyClickHandlers();
@@ -12,7 +13,7 @@ function initializeApp(){
 
 function applyClickHandlers(){
   $('#number-block').on('click', '.number', function numberButtonHandler(event){
-    console.log(event);
+    // console.log(event);
     inputtedNumber = $(event.currentTarget).find('p').text();
     stringNumberToPush = stringNumberToPush.concat(inputtedNumber);
     displayArray.push(inputtedNumber);
@@ -20,25 +21,60 @@ function applyClickHandlers(){
     updateDisplay();
   });
   $('#operator-column').on('click', '.operator', function operatorButtonHandler(event){
-    console.log(event);
+    // console.log(event);
     var inputtedOperator = "";
     inputtedOperator = $(event.currentTarget).find('p').text();
     displayArray.push(inputtedOperator);
     updateDisplay();
-    calculationArray.push(stringNumberToPush);
+    if (stringNumberToPush != "") {
+      calculationArray.push(stringNumberToPush);
+    }
+    //given input in calculator [2, +, -, *, 2]
+    //we are checking if the last-inputted operator is an operator (so not a number)
+    //this will work with any sequence of operators because it will always just replace w/ the last inputted operator
+    //if not, assigns it to the last-inputted operator and then does the calculation
+    if (isNaN(calculationArray[calculationArray.length-1])) {
+        calculationArray[calculationArray.length-1] = inputtedOperator;
+      }
+      else {
+        calculationArray.push(inputtedOperator);
+      }
     console.log(calculationArray);
-    calculationArray.push(inputtedOperator);
     stringNumberToPush = "";
   });
   $('#equals').on('click', function equalsButtonHandler(event){
     calculationArray.push(stringNumberToPush);
     stringNumberToPush = "";
     displayArray = [];
-    var answer = calculate(calculationArray[0], calculationArray[2], calculationArray[1]);
+    answer = calculate(calculationArray[0], calculationArray[2], calculationArray[1]);
     console.log(answer);
     displayArray.push(answer);
     updateDisplay();
   });
+  $('#decimal').on('click', function decimalsButtonHandler(event){
+    var inputtedDecimal = "";
+    inputtedDecimal = $(event.currentTarget).find('p').text();
+    stringNumberToPush = stringNumberToPush.concat(inputtedDecimal);
+    displayArray.push(inputtedDecimal);
+    updateDisplay();
+    if (stringNumberToPush.includes('..')){
+      stringNumberToPush = stringNumberToPush.substring(0, stringNumberToPush.length-1);
+    }
+  });
+  $('#ac-button').on('click', function clearCalculator(event){
+    stringNumberToPush = "";
+    calculationArray = [];
+    inputtedNumber = "";
+    answer = null;
+    displayArray = [];
+    updateDisplay();
+  });
+  // $('#c-button').on('click', function clearOnce(event){
+    //remove from end of string every time it is clicked
+  //   stringNumberToPush = $(stringNumberToPush).slice(0,-1);
+  //   displayArray.push(stringNumberToPush);
+  //   updateDisplay();
+  // });
 }
 
 function updateDisplay (){
@@ -62,6 +98,10 @@ function calculate (num1, num2, operator){
       break;
     case "/":
       result = number1/number2;
+  }
+  switch (number2) {
+    case 0:
+      result = "Ewwor (ಥ﹏ಥ)";
   }
   return result;
 }
